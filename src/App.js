8adfa10d82d8ef2096ac24a2484dd4e31c26f4fb
Saquib <mohdsaquib9876@gmail.com>
2020-12-23 16:32:26 +0530

    change

--- src/App.js
+++ src/App.js
@@ -3,29 +3,38 @@ import Login from "./login";
 import { HashRouter as Router, Route, Link } from "react-router-dom";
 import Dashboard from "./dashboard";
 import Register from "./register";
+import { Component } from "react";
+const { ipcRenderer: ipc } = window.require("electron");
 
-function App() {
-    return (
-        <div className="App">
-            <Router>
-                <Route path="/" exact component={Dashboard} />
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-                <button>
-                    <Link to="/">Dashboard</Link>
-                </button>
-                <button>
-                    <Link to="/login">Login</Link>
-                </button>
-                <button>
-                    <Link to="/register">Register</Link>
-                </button>
-                <button>
-                    <Link to="/update">Update Available</Link>
-                </button>
-            </Router>
-        </div>
-    );
+class App extends Component {
+    componentDidMount() {
+        ipc.on("message", (evt, text) => {
+            console.log(text);
+        });
+    }
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <Route path="/" exact component={Dashboard} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                    <button>
+                        <Link to="/">Dashboard</Link>
+                    </button>
+                    <button>
+                        <Link to="/login">Login</Link>
+                    </button>
+                    <button>
+                        <Link to="/register">Register</Link>
+                    </button>
+                    <button>
+                        <Link to="/update">Update Available</Link>
+                    </button>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-const { ipcRenderer: ipc } = window.require("electron");
+
 const Dashboard = () => {
     return (
         <div>
-            {ipc.send("dashboard", "hello")}
             <h1>Dashboard</h1>
         </div>
     );
